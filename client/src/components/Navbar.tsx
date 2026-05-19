507926db241f47a8972de6f144a312d9603a1b42
vanshikamudgal30 <vanshika.mudgal@startappss.com>
2026-05-19 14:12:31 +0530

    Added changes in navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -22,27 +22,31 @@ export default function Navbar() {
   return (
     <header className="sticky top-0 z-50 border-b border-[#6e4a41] bg-[#5b342b]/95 backdrop-blur-xl shadow-[0_4px_18px_rgba(0,0,0,0.25)]">
       <nav className="mx-auto flex max-w-7xl items-center justify-between px-4 py-4">
+
         <Link
           href="/"
-          className="group flex items-center gap-2 text-2xl font-bold !text-white"
+          className="flex items-center gap-2 text-2xl font-bold text-white"
         >
-          <BookOpen className="h-6 w-6 !text-white" />
-          <span className="!text-white">BookNest</span>
+          <BookOpen className="h-6 w-6 text-white" />
+          BookNest
         </Link>
 
         <div className="flex items-center gap-5 text-sm font-medium text-white">
+
+          <Link href="/">Home</Link>
+
           <Link href="/dashboard">Dashboard</Link>
 
           <Link href="/books">Books</Link>
 
           <Link href="/shelf">Shelf</Link>
 
-          <button
-            onClick={handleLogout}
-            className="rounded-xl bg-red-600 px-5 py-2 text-white"
-          >
-            Logout
-          </button>
+          <Link href="/login" className="  text-white">Login</Link>
+
+          <Link href="/signup" className="text-[#4E2F28] font-semibold">Sign Up</Link>
+
+          <button onClick={handleLogout} className="rounded-xl bg-red-600 px-5 py-2 text-white">Logout</button>
+
         </div>
       </nav>
     </header>
